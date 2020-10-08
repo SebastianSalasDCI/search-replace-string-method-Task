ee2d25babf903e2bf471af4140024b69da2022ed
SebastianSalasDCI <55529652+SebastianSalasDCI@users.noreply.github.com>
2020-10-08 12:50:42 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+//Select the paragraph, inputs and alerts from the html document
+
+
+//Assign the style display = none to prevent the alert to show from beggining.
+
+
+//create function for search
+function search() {
+
+  // obatain the innerHTML from the paragraph
+
+
+  // obatain the value from the inputSearch
+  
+
+  // use the search method to search
+  
+
+  // change the display style from the alert for it to appear
+  
+
+  // use an if statement to check if the word was found in the text or 
+  // not and assign the respective alert
+
+
+}
+
+function replace() {
+
+  // obatain the innerHTML from the paragraph
+  
+
+  // obatain the value from the inputSearch and the inputReplace
+  
+
+  // change the display style from the alert for it to appear
+  
+
+  // Check if the searchWord is empty with an if-Statement. Then check if
+  // the word was replaced or not and assign the respective alert
+  
+
+}
+
+
